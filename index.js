87cd95a460bde145aea15b997c1112ef3dcd073f
Altered Art <cherokeedreemer@gmail.com>
2020-06-09 10:32:19 -0600

    adding first steps

---
+++ index.js
@@ -0,0 +1,19 @@
+
+function greet() {
+return "hello world!";
+}
+console.log(greet);
+
+let name = "waffles";
+console.log(name);
+
+
+const intrestRate = 0.3;
+
+console.log(intrestRate);
+
+
+let age = 30;
+let isApproved = false;
+let happy = undefined;
+let color = null;
