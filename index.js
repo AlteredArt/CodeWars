98276a901b1e2cd6a2113da5c6ae6eb6071ae3aa
Altered Art <cherokeedreemer@gmail.com>
2020-06-12 13:16:43 -0600

    phone number code war

--- index.js
+++ index.js
@@ -37,3 +37,8 @@ var ageVer = function(age){
   }
 }
  ageVer(9)
+
+
+ function createPhoneNumber(numbers){
+   return `(${numbers.slice(0,3).join('')}) ${numbers.slice(3,6).join('')}-${numbers.slice(6).join('')}`;
+ }
